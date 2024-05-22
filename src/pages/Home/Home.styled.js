e8f29ec93536c29486fb23a6269eb7b296a7d1dd
Shkliarova <shklyarovaalisa@gmail.com>
2024-05-22 21:06:55 +0300

    Update Home.styled.js

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -2,7 +2,12 @@ import styled from "styled-components"
 
 export const HomeWrap = styled.div`
 text-align: center;
-margin-top: 15%;`
+margin: 200px auto;
+width: calc(100% - 30px);
+
+@media screen and (min-width: 740px) {
+    margin: 250px auto;
+  }`
 
 export const HomeText = styled.p`
 font-size: larger;
