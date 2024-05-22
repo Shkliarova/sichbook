b18ec15ace80c0de873340a8006fca388e48f7bf
Shkliarova <shklyarovaalisa@gmail.com>
2024-05-22 21:09:45 +0300

    Update NotFound.styled.js

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 export const NotFoundText = styled.p`
 text-align: center;
-margin-top: 20%;
-font-size: x-large;`
+margin: 200px auto;
+width: calc(100% - 30px);
+font-size: x-large;
+
+@media screen and (min-width: 740px) {
+    margin: 250px auto;
+  }`
