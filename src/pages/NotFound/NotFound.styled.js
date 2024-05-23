8be8a4e7d1d34a7a382758e3e485f6f7509c04f2
Shkliarova <shklyarovaalisa@gmail.com>
2024-05-23 18:08:34 +0300

    responsive layout for not found page

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -2,17 +2,20 @@ import styled from "styled-components";
 
 export const NotFoundText = styled.p`
 text-align: center;
-/* margin: 250px auto; */
+margin: 0px auto;
 width: calc(100% - 30px);
 font-size: x-large;
-
-@media screen and (min-width: 740px) {
-    /* margin: 300px auto; */
-  }`
+`
 
 export const StyledSvg = styled.svg`
-margin: 40px auto;
+margin: 124px auto;
 margin-bottom: 0px !important;
 display: block;
-width: 550px;
-height: 550px;`
+width: 300px;
+height: 300px;
+
+@media screen and (min-width: 740px) {
+    width: 550px;
+    height: 550px;
+    margin: 40px auto;
+  }`
