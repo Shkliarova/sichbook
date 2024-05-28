53dbf87bbaea59f5a5a8f66a3295b5c9eb996f49
Shkliarova <shklyarovaalisa@gmail.com>
2024-05-28 10:40:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import { GlobalStyle } from './components/GlobalStyled';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename="/sichbook">
         <App />
-      </HashRouter>
+      </BrowserRouter>
       </Provider>
     </PersistGate>
     <GlobalStyle/>
